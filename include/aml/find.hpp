ce4d9659c23d37fd000382aed695ca87fa259a24
___ <___>
2021-05-19 00:59:42 +0200

    missing typename error (detected by clang) has been corrected

--- include/aml/find.hpp
+++ include/aml/find.hpp
@@ -44,7 +44,7 @@ namespace aml
             
             using type =
                 
-                eval<
+        typename        eval<
                 typename
             std::conditional_t
             <

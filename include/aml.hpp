b3396e5f4a05e70d80f69c8188ee60345702d4a4
___ <___>
2021-05-25 01:21:54 +0200

    .

---
+++ include/aml.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "./aml/apply.hpp"
+#include "./aml/basic_types.hpp"
+#include "./aml/conslist.hpp"
+#include "./aml/exponent.hpp"
+#include "./aml/find.hpp"
+#include "./aml/linker.hpp"
+#include "./aml/partition.hpp"
+#include "./aml/select_indices.hpp"
+#include "./aml/sort.hpp"
+#include "./aml/string.hpp"

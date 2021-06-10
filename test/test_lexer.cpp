8b4c9deae4787efd1410be5e56b6dfed013ad927
Andreas Milton Maniotis <andreas.maniotis@gmail.com>
2021-06-10 23:14:39 +0200

    Tidying up in lexer. lexer still has a bug

--- test/test_lexer.cpp
+++ test/test_lexer.cpp
@@ -1,93 +1,101 @@
 #include "aml/dictionary.hpp"
 #include "aml/exponent.hpp"
+#include "aml/set.hpp"
 
 
-namespace act //aml::compiler::tokenization
+namespace aml::compiler::tokenization
 {
-    using namespace aml;
 
-    template
-    <
-        typename                           Target,
-        template<typename... Letter> class Source_Target_Predicate
-    >
-    struct target
+    template<typename Node, template<typename...> class Label_Predicate>
+    struct target;
+
+
+    template< typename... >
+    struct arrows;
+
+
+    template< typename                       Source_Node
+            , typename...                    Target_Node
+            , template<typename...> class... Label_Predicate
+            >
+    struct arrows<Source_Node, target<Target_Node, Label_Predicate>... >
     {
-        using node = Target;
+        //    private:
+        template<typename...>
+        friend struct dfa;
 
-        template<typename Letter>
-        using is_target_for_letter  =  Source_Target_Predicate<Letter>;
+
+        using source_node  =  Source_Node;
+
+
+        template<typename Label>
+        struct bind
+        {
+            template<typename Encoded_Target>
+            using target_belongs_to_label  =  typename Encoded_Target::key::template apply_to<Label>;
+        };
+
+        // target_node<Label> = conslist<Node> or conslist<> if not found
+        template<typename Label>
+        using target_node  =  typename find
+                                  <
+            bind<Label>::template target_belongs_to_label
+            //            curry<1, label_matches_with_target>::template apply_to<Label>::template apply_to
+
+                                  >::
+                                  template in< entry< function< Label_Predicate >, Target_Node >... >::
+                                  template pointwise_apply< value >;
     };
 
 
-    template<typename...>
-    struct outgoing_arrows;
 
-    template
-    <
-        typename...                    Target_Node,
-        template<typename...> class... Source_Target_Predicate
-    >
-    struct outgoing_arrows< target<Target_Node, Source_Target_Predicate>...   >
+    template<typename...>
+    struct dfa;
+
+
+    template< typename     Start
+            , typename     Acceptor_Set
+            , typename...  Arrows
+            >
+    struct dfa< Start, Acceptor_Set, Arrows... >
     {
     private:
-        template<typename Letter>
-        struct first_satisfied_source_target_predicate
+        using transition_map  =  dictionary< entry<typename Arrows::source_node, Arrows>... >;
+
+        struct no_node_found
         {
-            template<typename Target>
-            using apply_to = typename Target::template is_target_for_letter<Letter>;
+            template<typename>
+            using target_node = conslist<>;
+        };
+
+        template< typename Label >
+        struct fix_label
+        {
+            template< typename Arrows_ >
+            using target_node = typename Arrows_::template target_node<Label>;
         };
 
     public:
-        template<typename Letter>
-        using step  =  typename find< first_satisfied_source_target_predicate<Letter>::template apply_to >::template in< target<Target_Node, Source_Target_Predicate>... >::head::node;
 
-        using list = conslist<target<Target_Node, Source_Target_Predicate>...>;
+        using start      =  Start;
+        using acceptors  =  Acceptor_Set;
+
+        template< typename Node
+                , typename Label
+                >
+        using transition  =  typename transition_map::
+                             template partial_lookup<Node>::
+                             head::
+                             template pointwise_apply< fix_label<Label>::template target_node >;
+
+        // transition<N, L> == conslist<P>, iff N-->P is the first edge to satisfy the label L.
+        // transition<N, L> == conslist<>,  iff such and edge does not exist.
     };
 
 
-    struct erratic_token
-    {
-    };
-
-
-    template<typename Node, typename... Target  >
-    using arrows = entry<Node, outgoing_arrows<Target... > >;
-
-
-    template<typename...>
-    struct automaton;
-
-
-    template
-    <
-        typename... Source_Node,
-        typename... Outgoing_Arrows  // = outgoing_arrows<Target...>
-    >
-    struct automaton<entry<Source_Node, Outgoing_Arrows>... >
-    {
-    private:
-        template<typename Outgoing_Arrows_>
-        using add_arrow_to_erratic_token_to  =  typename Outgoing_Arrows_::
-                                                list::
-                                                template rcons< target<erratic_token, top > >::
-                                                template apply<outgoing_arrows>;
-
-        template<typename Sentinel>
-        using arrow_from_erratic_token_to  =  arrows<erratic_token,    outgoing_arrows<target<Sentinel, top> > >;
-
-    public:
-        using sentinel        =  head< conslist<Source_Node...> >;
-
-        using transition_map  =  typename conslist< entry< Source_Node, add_arrow_to_erratic_token_to<Outgoing_Arrows> >... >
-                                 ::template rcons<arrow_from_erratic_token_to<sentinel> >
-                                 ::template apply<dictionary>;
-
-
-    };
-
-
-    template<typename Type, typename Value>
+    template< typename Type
+            , typename Value
+            >
     struct token
     {
         using type  = Type;
@@ -95,128 +103,124 @@ namespace act //aml::compiler::tokenization
     };
 
 
-    template<typename Input,
-             typename Automaton,
-             typename Sentinel  = typename Automaton::sentinel,
-             typename TokenType = Sentinel,
-             typename Scanned   = conslist<>,
-             typename Output    = conslist<>>
-    struct lexer_step;
+    struct unfinished_token;
 
 
-    // parser has finished  correctly
-    template<typename Automaton,
-             typename Sentinel,
-             typename Scanned,
-             typename Output>
-    struct lexer_step<conslist<>,  Automaton,  Sentinel,  Sentinel,  Scanned,  Output>
+    template< typename Token
+            , typename Value
+            >
+    struct token_error
     {
-        static_assert(Scanned::size() == 0, "Internal Error: Parser appears to be ready, but unprocessed input remains.");
-        using type   =  lexer_step<  conslist<>,    Automaton,    Sentinel,    erratic_token,    Scanned,    Output>;
-        using output = Output;
+        using type   =  Token;
+        using value  =  Value;
     };
 
 
-    // parser has finished too early:
-    template<typename Automaton,
-             typename Sentinel,
-             typename TokenType, // not sentinel!
-             typename Scanned,
-             typename Output>
-    struct lexer_step<conslist<>,  Automaton,  Sentinel,  TokenType,  Scanned,  Output>
-    {
-        static_assert( is_same<TokenType, Sentinel>::eval() == false, "");
-        using type  =  lexer_step<  conslist<>,    Automaton,    Sentinel,    erratic_token,    Scanned,    Output>;
-    };
-
-
-    // Parser has encountered an erratic token
-    template<typename Input,
-             typename Automaton,
-             typename Sentinel,
-             typename Scanned,
-             typename Output>
-    struct lexer_step<  Input,    Automaton,    Sentinel,    erratic_token,    Scanned,    Output>
-    {
-        static_assert(Input::size() != 0, "");
-        using type  =  lexer_step<  conslist<>,    Automaton,    Sentinel,    Sentinel,    conslist<>,    typename Output::template rcons<token<erratic_token, join<Scanned, Input> > > >;
-    };
-
-
-    // parser has encountered an erratic token and the input is empty
-    template<typename Automaton,
-             typename Sentinel,
-             typename Scanned,
-             typename Output
-             >
-    struct lexer_step<  conslist<>,    Automaton,    Sentinel,    erratic_token,    Scanned,    Output  >
-    {
-        using type  =  lexer_step<  conslist<>,    Automaton,    Sentinel,    erratic_token,    Scanned,    typename Output::template rcons<token<erratic_token, Scanned> > >;
-    };
-
-
-    template<  typename Input,
-               typename Automaton,
-               typename Sentinel,
-               typename TokenType,
-               typename Scanned,
-               typename Output  >
+    template< typename dfa
+            , typename Input
+            , typename Node
+            , typename Scanned
+            , typename Output
+            >
     struct lexer_step
     {
-    private:
-        static_assert( Input::size() > 0, "");
-        static_assert( is_same<TokenType, erratic_token>::eval() == false, "" );
+        // 1) Input == conslist<> and Scanned != conslist<> indicates an error (unfinished token), abnormal termination
+        // 2) Input == conslist<> and Scanned == conslist<> is the condition of normal termination
+        // 3) Input != conslist<> indicates that the parser is runing:
+        //
+        //      subcase 3a) no transition exists and the state "Node" is conctained in dfa::acceptors:
+        //                  token is written to output
+        //      subcase 3b) no transition exists and the state "Note" is not contained in dfa::acceptors:
+        //                  The token token_error<Node, Scanned> is written to output.
+        //      subcase 3c) Node is a non-final state and the input stream is not empty. Then the Scanned is updated.
 
-        using next_node   =  typename Automaton::
-                             transition_map::
-                             template lookup<TokenType>::
-                             head::                     // type of head: outgoing_nodes<target<a_node, a_predicate>... >
-                             template step<typename Input::head>;
-
-        using input_head  =  head<Input>;
-        using input_tail  =  tail<Input>;
-
-
-        struct write_out
+        struct terminate_with_unfinished_token
         {
-            using updated_output = typename Output::template rcons<token<TokenType, Scanned> >;
+            using output_  =  typename Output::template rcons<token_error<unfinished_token, Scanned > >;
 
-            using type  =  lexer_step< Input,    Automaton,    Sentinel,    Sentinel,     conslist<>,    updated_output >;
+            using type = lexer_step< dfa, conslist<>, typename dfa::start, conslist<>, output_ >;
+        };
+
+        struct terminate
+        {
+            //            using type   = lexer_step<dfa, conslist<>, dfa::start, conslist<>, Output>;
+            using output  =  Output;
+        };
+
+        struct write_token_error
+        {
+            using output_  =  typename Output::template rcons< token_error< Node, Scanned > >;
+
+            using type = lexer_step<dfa, Input, typename dfa::start, conslist<>, output_>;
         };
 
 
-        struct scan_next
+        struct write_valid_token
         {
-            using updated_scan = typename Scanned::template rcons<input_head>;
+            using output_  =  typename Output::template rcons< token< Node, Scanned > > ;
 
-            using type  =  lexer_step< input_tail,    Automaton,    Sentinel,    next_node,    updated_scan,    Output >;
+            using type = lexer_step<dfa, Input, typename dfa::start, conslist<>, output_>;
         };
 
 
-    public:
-        using type  =  typename is_same<next_node, Sentinel>::
-                       template conditional<write_out, scan_next >::
-                       type;
+        struct consume_symbol
+        {
+            using symbol_   =  head< Input >;
+            using node_     =  typename dfa::template transition< Node, symbol_ >::head;
+            using input_    =  tail<Input>;
+            using scanned_  =  typename Scanned::template rcons< symbol_ >;
 
+            using type = lexer_step< dfa, input_, node_, scanned_, Output >;
+        };
+
+
+        struct final_step
+        {
+            using type = typename conditional< bool_<Scanned::size() == 0>,
+                                               terminate,
+                                               terminate_with_unfinished_token >::type;
+        };
+
+
+        struct intermediate_step
+        {
+            using symbol_      =  head< Input >;
+            using transition_  =  typename dfa::template transition< Node, symbol_ >;
+
+            using type  =  typename conditional
+                                    <
+                                        typename dfa::acceptors::template contains<Node>,
+
+                                        write_valid_token,
+
+                                        conditional
+                                        <
+                                            is_same<transition_, conslist<> >,
+
+                                            write_token_error,
+
+                                            consume_symbol
+                                        >
+                                    >::type;
+        };
+
+        using type = typename conditional< bool_< Input::size() == 0 >, final_step, intermediate_step >::type;
     };
 
 
-    template<typename Automaton, typename Input>
-    using lexer  =  power< exp<1>, lexer_step<Input, Automaton> >;
-
-        //lexer_step<Input, Automaton>;
-        //typename lexer_step<Input, Automaton>::type;
+    template<typename dfa, typename Input>
+    using lexer  =  //typename power< exp<infinity>, lexer_step<dfa, Input, typename dfa::start, conslist<>, conslist<> > >::output;
+        typename lexer_step<dfa, Input, typename dfa::start, conslist<>, conslist<> >::type;
 
 }
 
 
-//using namespace aml::compiler::tokenization;
-using namespace act;
 
+using namespace aml::compiler::tokenization;
 
+struct start_t;
 struct one_t;
 struct semicolon_t;
-struct sentinel_t;
 
 
 template<typename X>
@@ -233,13 +237,14 @@ struct is_semicolon
 };
 
 
+using start_arrows = arrows<start_t, target<one_t, is_one>, target<semicolon_t, is_semicolon> >;
+using arrows_from_one_t = arrows<one_t, target<one_t, is_one> >;
+using arrows_from_semicolon_t = arrows<semicolon_t, target<semicolon_t, is_semicolon> >;
 
-using sentinel_arrows        = arrows<sentinel_t,  target<one_t, is_one>,       target<semicolon_t, is_semicolon> >;
-using node_one_arrows        = arrows<one_t,       target<one_t, is_one>,       target<sentinel_t,  is_semicolon> >;
-using node_semicolon_arrows  = arrows<semicolon_t, target<sentinel_t, is_one>,  target<sentinel_t,  is_semicolon> >;
+using dfa_t = dfa<  start_t,    aml::set<semicolon_t, one_t>,    start_arrows,
+                                                                 arrows_from_one_t,
+                                                                 arrows_from_semicolon_t >;
 
-
-using automaton_t  =  automaton< sentinel_arrows, node_one_arrows, node_semicolon_arrows >;
 
 #include "aml/string.hpp"
 
@@ -248,12 +253,11 @@ using aml::operator""_;
 template<auto... objects>
 using make_type_list = aml::conslist< aml::object<objects>... >;
 
-//using listing = decltype("22323333222"_)::apply<make_type_list>;
-using listing = decltype("1;"_)::apply<make_type_list>;
 
-using s = typename automaton_t::sentinel;
+using listing = decltype("1"_)::apply<make_type_list>;
 
-using tokenized = lexer<automaton_t, listing>;
+
+using tokenized = lexer<dfa_t, listing>;
 
 
 #include <boost/core/demangle.hpp>
@@ -262,7 +266,5 @@ using tokenized = lexer<automaton_t, listing>;
 int main()
 {
     std::cout << boost::core::demangle(typeid(tokenized).name()) << std::endl;
-    //    automaton_t::sentinel* ptr;
-    //    std::cout << boost::core::demangle(typeid(tokenized).name()) << std::endl;
-    
+
 }

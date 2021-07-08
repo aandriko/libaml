af2c02eab1e4589e116a618e476a8b5fd5dbd8b8
Andreas Milton Maniotis <andreas.maniotis@gmail.com>
2021-07-08 17:39:20 +0200

    dictionary: some corrections + some tests

--- aml-2/include/aml/dictionary.hpp
+++ aml-2/include/aml/dictionary.hpp
@@ -28,23 +28,38 @@ namespace aml
     };
 
 
+    template<typename... Sentinel>
+    struct sentinel
+    {
+        static_assert(sizeof...(Sentinel) <= 1);
+    };
+
+    template<typename Sentinel>
+    struct sentinel<Sentinel>
+    {
+        using key    =  Sentinel;
+        using value  =  Sentinel;
+    };
+
+
     template<typename... Entries>
     struct dictionary;
 
 
-    template<  typename... Key
+    template<  typename... Sentinel
+            ,  typename... Key
             ,  typename... Value
             >
-    struct dictionary< entry<Key, Value>... >
+    struct dictionary<  sentinel< Sentinel... >, entry< Key, Value >...  >
     :
         private set<Key...>  // ensure that keys are unique
     {
-        using keys       =  aml::list<  Key...  >;
-        using values     =  aml::list<  Value...  >;
-        using entry_list =  aml::list<  entry<Key, Value>...  >;
+        using key_list    =  aml::list<  Key...  >;
+        using value_list  =  aml::list<  Value...  >;
+        using entry_list  =  aml::list<  entry< Key, Value >...  >;
 
-        using entry_set  =  aml::set< entry<Key, Value>... >;
-        using key_set    =  aml::set< Key... >;
+        using entry_set   =  aml::set< entry<Key, Value>... >;
+        using key_set     =  aml::set< Key... >;
 
     private:
 
@@ -57,56 +72,48 @@ namespace aml
 
     public:
 
+        template< typename... S >
+        using with_sentinel  =  dictionary<  sentinel<S...>, entry< Key, Value >...  >;
+
+
         template< typename Key_ >
         using at  =  typename entry_list::
-                     template  split_by_first_occurence_of< key_matches_entry<Key_>::
+                     template split_by_first_occurence_of< key_matches_entry<Key_>::
                                                            template apply_to >::
                      suffix::
+                     template rcons< sentinel<Sentinel>...>::
                      head::
                      value;
-
-
-        template< typename Key_ >
-        using relaxed_at  =  typename entry_list::
-                             template  split_by_first_occurence_of< key_matches_entry<Key_>::
-                                                                    template apply_to >::
-                             suffix::
-                             head::
-                             value;
 
 
         template< typename... Key_ >
         using map_to_values  =  aml::list< at<Key_>... >;
 
 
-        template<  typename... Key_  >
-        using relaxed_map_to_values  =  aml::list< relaxed_at< Key_ >... >;
-
-
     private:
 
         template<  typename List
                 ,  typename Entry
-                ,  typename split  =  typename entry_list::
+                ,  typename split  =  typename List::
                                       template split_by_first_occurence_of<
                        key_matches_entry< typename Entry::key >::template apply_to >
                >
         using add_entry_to_list  =  typename split::
                                     prefix::
                                     template rcons< Entry >::
-                                    template apply< split::postfix::template cons >;
+            template apply< split::suffix::relaxed_tail::template cons >;
 
 
         template<  typename List
                 ,  typename Key_
-                ,  typename split  =  typename entry_list::
+                ,  typename split  =  typename List::
                                       template split_by_first_occurence_of<
                        key_matches_entry< Key_ >::template apply_to >
                >
         using remove_key_from_list  =  typename split::
                                        prefix::
                                        template apply< split::
-                                                       postfix::
+                                                       suffix::
                                                        relaxed_tail::
                                                        template cons >;
 
@@ -115,17 +122,31 @@ namespace aml
 
         template<typename... New_Entries>
         using add_entries  =  typename aml::list< New_Entries... >::
-                              template lfold_with< add_entry_to_list, list >::
-                              template apply< dictionary >;
+                              template lfold_with< add_entry_to_list, entry_list >::
+                              template apply< dictionary >::
+                              template with_sentinel< Sentinel... >;
+
 
 
         template<typename... Remove_Keys>
         using remove_keys  =  typename aml::list< Remove_Keys... >::
-                              template lfold_with< remove_key_from_list, list >::
-                              template apply< dictionary >;
+                              template lfold_with< remove_key_from_list, entry_list >::
+                              template apply< dictionary >::
+                              template with_sentinel< Sentinel... >;
 
 
         static constexpr auto size() { return sizeof...(Key); }
 
     };
+
+    template<  typename... Key
+            ,  typename... Value
+            >
+    struct dictionary<  entry< Key, Value >...  >
+    :
+        public dictionary<  sentinel<>, entry< Key, Value >...  >
+    { };
+
+
 }
+
--- aml-2/test/test_dictionary.cpp
+++ aml-2/test/test_dictionary.cpp
@@ -21,7 +21,80 @@ namespace test::dictionary
 {
     void test_empty_dictionary()
     {
+        using d0 = aml::dictionary<>;
+
+        static_assert( d0::size() == 0 );
+        static_assert( std::is_same< d0::key_list,   aml::list<> >::value );
+        static_assert( std::is_same< d0::value_list, aml::list<> >::value );
+        static_assert( std::is_same< d0::entry_list, aml::list<> >::value );
+        static_assert( std::is_same< d0::entry_set,  aml::set<> >::value );
+        static_assert( std::is_same< d0::key_set,    aml::set<> >::value );
+
+        using d0s  =  aml::dictionary<>::with_sentinel<char***>;
+        static_assert( std::is_same< d0s::at<int>, char***>::value );
+
+        using l0 = d0::map_to_values<>;
+        static_assert( std::is_same< aml::list<>, l0 >::value );
+
+        using l0s  =  d0s::map_to_values<>;
+        static_assert( std::is_same< aml::list<>, l0s >::value );
+
+        using l1  =  d0s::map_to_values<int, double, char***>;
+        static_assert( std::is_same< aml::list<char***, char***, char***>, l1 >::value );
+
+        using l2 = d0::add_entries<  aml::entry< int, double >
+                                  ,  aml::entry< int, int* >
+                                  ,  aml::entry< double, void >
+                                  >;
+
+        using l2_list = aml::list< aml::entry<int, int*>, aml::entry<double, void> >;
+        static_assert( std::is_same< l2::entry_list, l2_list >::value );
     }
+
+
+    struct sentinel_t;
+
+
+    void test_type_with_sentinel()
+    {
+        using d1   =  aml::dictionary< aml::entry<int*, double > >;
+        using d1_s  =  aml::dictionary< aml::sentinel<sentinel_t>, aml::entry<int*, double> >;
+
+        static_assert( std::is_same< d1::with_sentinel<sentinel_t>, d1_s >::value );
+    }
+
+    template< typename Dictionary
+            , typename Key
+            , typename  = typename Dictionary::template at<Key>
+            >
+    aml::true_ at_is_successful_( std::nullptr_t, std::nullptr_t );
+
+
+    template< typename, typename >
+    aml::false_ at_is_successful_( std::nullptr_t, ... );
+
+
+    template< typename Dictionary
+            , typename Key >
+    using at_is_successful  =  decltype( at_is_successful_<Dictionary, Key>(nullptr, nullptr));
+
+    void test_at()
+    {
+        using d = aml::dictionary< aml::entry<int, double >,
+                                   aml::entry<int*, double*>,
+                                   aml::entry<int**, double**> >;
+
+
+        using ds = d::with_sentinel<sentinel_t>;
+
+
+        static_assert( at_is_successful< d,  int* >::eval() == true );
+        static_assert( at_is_successful< ds, int* >::eval() == true );
+
+        static_assert( at_is_successful< d, long >::eval() == false );
+        static_assert( at_is_successful<ds, long >::eval() == true  );
+    }
+
 }
 
 

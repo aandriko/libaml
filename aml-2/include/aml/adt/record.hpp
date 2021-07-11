57ee52927a7bbefd5d06064675a4d2f3a1f4c40a
Andreas Milton Maniotis <andreas.maniotis@gmail.com>
2021-07-11 02:30:20 +0200

    adt/record: improvement and completion of interface + more unit-tests

--- aml-2/include/aml/adt/record.hpp
+++ aml-2/include/aml/adt/record.hpp
@@ -27,20 +27,39 @@ namespace aml::adt
 
     public:
 
-        template<typename... Args>
-        constexpr indexed_type( Args&&... args )  noexcept( noexcept( Type( static_cast<Args&&>(args)... ) ) )
+        template<  typename... Args
+                ,  typename  =  typename  // exclude copy and move construction
+                                none<  is_same<  list< Args... >, list< this_type const & >  >
+                                    ,  is_same<  list< Args... >, list< this_type & >  >
+                                    ,  is_same<  list< Args... >, list< this_type && > >
+                                    >::sfinae
+                >
+        explicit constexpr indexed_type( Args&&... args )  noexcept( noexcept( Type( static_cast<Args&&>(args)... ) ) )
         :
             type_( static_cast< Args&& >(args)... )
         { }
 
-        constexpr indexed_type& operator=( indexed_type const& )  =  default;
-        constexpr indexed_type& operator=( indexed_type&&      )  =  default;
 
-        Type const& cref() const  noexcept  { return type_; }
-        Type const& ref()  const  noexcept  { return cref(); }
-        Type &      ref()         noexcept  { return type_; }
-        Type &&     rref()        noexcept  { return type_; }
+        constexpr
+        indexed_type (indexed_type const& other )  noexcept( noexcept( Type( other.type_) ) )
+        :    type_( other.type_ )
+        { }
 
+
+        constexpr
+        indexed_type( indexed_type&& other )  noexcept( noexcept( Type( static_cast< Type&& >( other.type_) ) ) )
+        :     type_( static_cast< Type&& >(other.type_) )
+        { }
+
+        constexpr indexed_type& operator=( indexed_type const& )   =  default;
+
+        constexpr indexed_type& operator=( indexed_type&&  other)  =  default;
+
+
+        Type const &  cref() const  noexcept  { return type_; }
+        Type const &  ref()  const  noexcept  { return cref(); }
+        Type &        ref()         noexcept  { return type_; }
+        Type &&       rref()        noexcept  { return static_cast<Type &&>(type_); }
     };
 
 
@@ -56,19 +75,41 @@ namespace aml::adt
         template<typename Entry_>
         using subtype  =  indexed_type< typename Entry_::key, typename Entry_::value >;
 
+        using this_type = record< Entry... >;
+
     public:
 
         template< template<typename... > class Less >
-        using reorder_with  =  typename list<Entry...>::template sort_with< Less >::template apply<record>;
+        using reorder_with  =  typename list<Entry...>::
+                               template sort_with< Less >::
+                               template apply<record>;
 
 
-        template< typename... Args >
-            constexpr record( Args&&... args )    noexcept( (noexcept(subtype<Entry>(static_cast< Args&& >(args) ) ) && ... ) ) 
-        :  subtype<Entry>(static_cast<Args&&>(args)) ...
+        template<  typename... Args
+                ,  typename  =  typename  // exclude copy and move construction
+                                none<  is_same<  list< Args... >, list< this_type const & >  >
+                                    ,  is_same<  list< Args... >, list< this_type & >  >
+                                    ,  is_same<  list< Args... >, list< this_type && > >
+                                    >::sfinae
+                >
+        explicit constexpr record( Args&&... args )
+            noexcept( (noexcept(subtype<Entry>(static_cast< Args&& >(args) ) ) && ... ) )
+        :  subtype<Entry>( static_cast< Args&& >(args)) ...
         { }
 
-        constexpr record& operator=(record const&) = default;
-        constexpr record& operator=(record&& )     = default;
+        constexpr record() = default;
+
+        constexpr record( record const& )           =  default;
+
+        constexpr record( record && other)  = default;
+            /*
+            noexcept( (noexcept(subtype<Entry>(static_cast< subtype<Entry>&& >(other) ) ) && ... ) )
+        :    subtype<Entry>( static_cast<subtype<Entry> &&>(other) )...
+        { }
+            */
+
+        constexpr record& operator=(record const&)  =  default;
+        constexpr record& operator=(record&& )      =  default;
 
 
         template< typename Key >
@@ -81,7 +122,8 @@ namespace aml::adt
         }
 
 
-        constexpr auto const& ref() const  noexcept { return cref(); }
+        template< typename Key >
+        constexpr auto const& ref() const  noexcept { return cref<Key>(); }
 
 
         template< typename Key >
@@ -90,7 +132,7 @@ namespace aml::adt
             using value_t  =  typename lookup_table::template lookup< Key >;
             using return_t = subtype<  entry< Key, value_t >  > &;
 
-            return static_cast< return_t >(*this).cref();
+            return static_cast< return_t >(*this).ref();
         }
 
 
@@ -100,23 +142,66 @@ namespace aml::adt
             using value_t  =  typename lookup_table::template lookup< Key >;
             using return_t =  subtype<  entry< Key, value_t >  > &&;
 
-            return static_cast< return_t >(*this).cref();
+            return static_cast< return_t >(*this).rref();
         }
 
 
         template< typename F >
-        decltype(auto) const_invoke(F&& f) const  noexcept( noexcept(std::invoke( std::declval<F>(),
-                                                                                  cref<typename Entry::key>()... ) ) )
+        decltype(auto) const_invoke(F&& f) const
+            noexcept( noexcept(std::invoke( std::declval<F>(), cref<typename Entry::key>()... ) ) )
         {
             return std::invoke( std::forward<F>(f), cref<typename Entry::key>()... );
         }
 
 
         template< typename F >
-        decltype(auto) move_invoke(F&& f)  const  noexcept( noexcept(std::invoke( std::declval<F>(),
-                                                                                  rref<typename Entry::key>()... ) ) )
+        decltype(auto) move_invoke(F&& f)  const
+            noexcept( noexcept(std::invoke( std::declval<F>(),  rref<typename Entry::key>()... ) ) )
         {
             return std::invoke( std::forward<F>(f), rref<typename Entry::key>()... );
+        }
+
+
+        friend constexpr bool operator==( record<Entry...> const& lhs,
+                                          record<Entry...> const& rhs )
+        {
+            return ( (lhs.cref<typename Entry::key>() == rhs.cref<typename Entry::key>()) && ... );
+        }
+
+
+        friend constexpr bool operator!=( record<Entry...> const& lhs,
+                                          record<Entry...> const& rhs )
+        {
+            return ( (lhs.cref<typename Entry::key>() != rhs.cref<typename Entry::key>()) && ...  && (sizeof...(Entry) != 0 ) );
+        }
+
+
+        friend constexpr bool operator<=( record<Entry...> const& lhs,
+                                          record<Entry...> const& rhs )
+        {
+            return ( (lhs.cref<typename Entry::key>() <= rhs.cref<typename Entry::key>()) && ...  );
+        }
+
+
+
+        friend constexpr bool operator>=( record<Entry...> const& lhs,
+                                          record<Entry...> const& rhs )
+        {
+            return ( (lhs.cref<typename Entry::key>() >= rhs.cref<typename Entry::key>()) && ... );
+        }
+
+
+        friend constexpr bool operator<( record<Entry...> const& lhs,
+                                         record<Entry...> const& rhs )
+        {
+            return ( (lhs.cref<typename Entry::key>() < rhs.cref<typename Entry::key>()) && ... && (sizeof...(Entry) != 0) );
+        }
+
+
+        friend constexpr bool operator>( record<Entry...> const& lhs,
+                                         record<Entry...> const& rhs )
+        {
+            return ( (lhs.cref<typename Entry::key>() > rhs.cref<typename Entry::key>()) && ... && (sizeof...(Entry) != 0) );
         }
     };
 }
--- aml-2/test/test_record.cpp
+++ aml-2/test/test_record.cpp
@@ -16,15 +16,190 @@
 #include <boost/core/demangle.hpp>
 
 #include <string>
+#include <memory>
 
+#include <cassert>
 
 namespace test::record
 {
     struct hello;
     struct world;
 
-    void test_0()
+
+    template<  typename T,
+               typename  =  decltype ( std::declval<T>() = std::declval<T const&>() ) >
+    aml::true_ has_copy_assignment_( std::nullptr_t, std::nullptr_t );
+
+
+    template<  typename T  >
+    aml::false_ has_copy_assignment_( std::nullptr_t, ... );
+
+
+    template<  typename T,
+               typename  =  decltype ( std::declval<T>() = std::declval<T&&>() ) >
+    aml::true_ has_move_assignment_( std::nullptr_t, std::nullptr_t );
+
+
+    template<  typename T  >
+    aml::false_ has_move_assignment_( std::nullptr_t, ... );
+
+
+    template<  typename T  >
+    using has_copy_assignment  =  decltype( has_copy_assignment_<T>(nullptr, nullptr) );
+
+
+    template<  typename T  >
+    using has_move_assignment  =  decltype( has_move_assignment_<T>(nullptr, nullptr) );
+
+
+    void test_indexed_type()
     {
+        using indexed_t = aml::adt::indexed_type<hello, std::unique_ptr<std::string>>;
+
+        static_assert( has_copy_assignment<indexed_t>::eval() == false );
+        static_assert( has_move_assignment<indexed_t>::eval() == true  );
+
+
+        indexed_t a( new std::string("hello") );
+        indexed_t b( new std::string("world"));
+
+
+        b = std::move(a) ;
+        a.ref() = std::make_unique<std::string>("hello again");
+
+        assert( *(a.ref()) == std::string("hello again") );
+
+        b = decltype(b)(a.rref());
+
+        assert( a.cref() == nullptr );
+
+
+        aml::adt::indexed_type<world, std::string> w1("hello");
+        aml::adt::indexed_type<world, std::string> w2("world");
+
+        aml::adt::indexed_type<world, std::string> w3(w2);
+
+        assert( static_cast<decltype(w2) const&>(w2).ref() == std::string("world") );
+
+        aml::adt::indexed_type<world, std::string> w4(std::move(w2));
+
+        assert( w2.ref() == "" );
+
+        w2 = w4;
+
+        assert( w2.ref() == "world" );
+
+        w2 = std::move(w4);
+
+        assert( w2.ref() == "world" );
+        assert( w4.ref() == "" );
+
+
+        static_assert( std::is_same< decltype(w2.ref()), std::string & >::value );
+        static_assert( std::is_same< decltype(w2.cref()), std::string const & >::value );
+        static_assert( std::is_same< decltype(w2.rref()), std::string && >::value );
+
+        static_assert( std::is_same< decltype(static_cast<decltype(w2) const & >(w2).ref()),
+                                     std::string const & >::value );
+    }
+
+    void test_record()
+    {
+        struct tag;
+
+        using r0_t    =  aml::adt::record<>;
+        using r1_t    =  aml::adt::record< aml::entry<hello, int> >;
+
+
+        using r3_a_t  =  aml::adt::record< aml::entry<hello, std::string>
+                                         , aml::entry<world, double >
+                                         , aml::entry<tag,   char >
+                                         >;
+
+        using r3_b_t =  aml::adt::record< aml::entry<hello, std::string>
+                                        , aml::entry<world, std::unique_ptr<double> >
+                                        >;
+
+        {
+            r0_t x0;
+            r0_t x1(x0);
+            r0_t x2( std::move(x0) );
+
+            assert(x2 == x1 && x2 == x0 );
+
+            assert( x0 == x1 );
+            assert( x0 <= x1 );
+            assert( x0 >= x1 );
+            assert( !( x0 != x1) );
+            assert( ! (x0 < x1 ) );
+            assert( ! (x0 > x1 ) );
+        }
+
+        {
+            r1_t y0;
+            assert( y0.ref<hello>() == 0 );
+
+            r1_t y1(2);
+            assert( y1.ref<hello>() == 2 );
+
+
+            assert( y0 < y1 );
+            assert( y0 <= y1 );
+            assert( !(y0 > y1) );
+            assert( !(y0 >= y1) );
+            assert( y0 != y1 );
+            assert( y0 == y0 );
+
+            y0 = std::move(y1);
+            assert (y1 == r1_t(2) );
+
+            y1 = r1_t(-1);
+
+            auto y2 = y1;
+
+            assert( y2.cref<hello>() == -1 );
+
+            assert( y2 == y1 );
+        }
+
+
+        {
+            r3_a_t z0( "hello", 2.4, 'a');
+
+
+            static_assert( has_copy_assignment<r3_a_t>::eval() == true );
+            static_assert( has_move_assignment<r3_a_t>::eval() == true );
+
+            static_assert( has_copy_assignment<r3_b_t>::eval() == false );
+            static_assert( has_move_assignment<r3_b_t>::eval() == true );
+
+            auto z1(z0);
+
+            assert(z1 == z0);
+            assert(! (z1 != z0) );
+
+            r3_a_t z2(std::move(z0));
+
+            //            assert(z2 != z0);
+            assert(! (z1 == z0) );
+
+            assert(z2 == z2 && z1 == z1 && z0 == z0 );
+
+
+            r3_b_t w0( "HELLO", new double(2.4));
+            r3_b_t w1( "xyz",   new double(-4.3));
+
+            r3_b_t w2;
+            assert(w2 == r3_b_t("", nullptr) );
+
+            std::string s("hello");
+            std::string t(std::move(s));
+
+            std::cout << s << ":: " << t << std::endl;
+
+        }
+
+
         aml::adt::record< aml::entry<hello, double*>, aml::entry<world, std::string> > r( nullptr, "hello");
 
         std::cout << r.cref<world>() << std::endl;
@@ -40,7 +215,8 @@ int main()
 {
     void (*test_set[])() =
     {
-        test::record::test_0,
+     test::record::test_indexed_type,
+     test::record::test_record,
     };
 
 

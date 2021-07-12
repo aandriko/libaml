c7bcc6837bd829a6020b3fcb52da43bc624ce701
Andreas Milton Maniotis <andreas.maniotis@gmail.com>
2021-07-13 01:39:14 +0200

    linker prototype: debugging of permutation invariant constructors

---
+++ aml-2/include/aml/string.hpp
@@ -0,0 +1,22 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2021 Andreas Milton Maniotis.
+//
+// Email: andreas.maniotis@gmail.com
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#pragma once
+
+#include "./object.hpp"
+
+namespace aml
+{
+
+    template<char... text>
+    using string = object<>::list<text...>;
+
+    template<typename Char=char, Char... txt> constexpr string<txt...>  operator ""_() { return { }; }
+}
